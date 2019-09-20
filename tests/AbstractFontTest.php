a4a0ef2f25f73a9ba4af903b3c56d1f93aff411c
FieryBeard <dm.denisov.mail@gmail.com>
2019-09-20 18:06:53 +0400

    Add kerning

--- tests/AbstractFontTest.php
+++ tests/AbstractFontTest.php
@@ -56,6 +56,13 @@ class AbstractFontTest extends TestCase
         $font->valign('top');
         $this->assertEquals('top', $font->valign);
     }
+    
+    public function testKerning()
+    {
+        $font = $this->getMockForAbstractClass('\Intervention\Image\AbstractFont');
+        $font->kerning(10.5);
+        $this->assertEquals(10.5, $font->kerning);
+    }
 
     public function testFile()
     {
